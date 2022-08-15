709c6ea17b61a67f8fd298b9884c60c3a0ae6520
Pete Fowler <fowler.pete@gmail.com>
2022-08-15 10:40:33 -0600

    Add disable/reenable other buttons on pause

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,9 @@
+const pauseBtn = document.querySelector('#pause');
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const heartBtn = document.querySelector('#heart');
+const submitBtn = document.querySelector('#submit');
+
 const counter = (() => {
   const counter = document.querySelector('#counter');
   let n = 0;
@@ -7,22 +13,59 @@ const counter = (() => {
     n++;
     counter.textContent = n;
   }
-  let nId = setInterval(count, 1000);
-  const pause = () => {
-    if(counting === true) {
-    clearInterval(nId);
-    counting = false;
-    } else {
-      nId = setInterval(count, 1000);
-      counting = true;
+  let int = setInterval(count, 1000);
+  const btnToggle = (status) => {
+    const btns = document.querySelectorAll('button');
+    if(status === 'disable') {
+      btns.forEach((btn) => {
+        if(btn.id !== 'pause') {
+          btn.disabled = true;
+        }
+      });
+    } else if(status === 'enable') {
+      btns.forEach((btn) => {
+        if(btn.id !== 'pause') {
+          btn.disabled = false;
+        }
+      });
     }
   }
-  return { pause };
+  const pause = () => {
+    if(counting === true) {
+      clearInterval(int);
+      counting = false;
+      pauseBtn.textContent = 'resume';
+      btnToggle('disable');
+    } else {
+      int = setInterval(count, 1000);
+      counting = true;
+      pauseBtn.textContent = 'pause';
+      btnToggle('enable');
+    }
+  }
+  const plus = () => {
+    let n = counter.textContent;
+    n++;
+    counter.textContent = n;
+    clearInterval(int);
+    int = setInterval(count, 1000);
+  }
+  const minus = () => {
+    let n = counter.textContent;
+    n--;
+    counter.textContent = n;
+    clearInterval(int);
+    int = setInterval(count, 1000);
+  }
+  return { pause, plus, minus };
 })();
 
 const listeners = (() => {
-  const pauseBtn = document.querySelector('#pause');
-  pauseBtn.addEventListener('click', counter.pause);
+pauseBtn.addEventListener('click', counter.pause);
+
+plusBtn.addEventListener('click', counter.plus);
+
+minusBtn.addEventListener('click', counter.minus);
 })();
 
 
